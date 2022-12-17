c22d8835eaf80b55ff9ce54020876ae45201bcb6
rfurkan37 <rfurkan37@gmail.com>
2022-12-17 17:10:51 +0300

    hw4 updated move func

--- hw241/hw4/util.cpp
+++ hw241/hw4/util.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <string>
+#include <memory>
 
 using namespace std;
 
@@ -12,10 +14,9 @@ class DayofYearSet
 			public:
 				DayofYear();// default constructor assigns 1 january
 				DayofYear(int m, int d);// constructor assigns month and day
-				void	input();// reads in month and day from keyboard
-				
 			private:
-				int	day, month;
+				int	day;
+				string month;
 			
 		};
 		DayofYearSet();// default constructor
@@ -30,15 +31,141 @@ class DayofYearSet
 		DayofYearSet		operator !	();
 		DayofYearSet		operator []	(int index);
 		void				remove(const DayofYear& element); // bu pointıra çevrilebilir.
-		int					size();
+		int					size() {return sizeofSet;};
 
 		
 	private:
 		DayofYear* dataSet;
-		int size;
-		int capacity;
+		int sizeofSet = 0;
+		int capacity = 4;
+		void reallocate();
+		void move_set(DayofYear* dest, DayofYear* src, size_t n);
 };
 
+DayofYearSet::DayofYear::DayofYear()
+{
+	day = 1;
+	month = "January";
+}
+
+DayofYearSet::DayofYear::DayofYear(int m, int d)
+{
+	if((d < 0 && d > 31) || (m < 0 && m > 12))
+	{
+		cerr << "Wrong date input !!!" << endl;
+		exit(1);
+	}
+	else if(m == 2 && d > 29)
+	{
+		cerr << "February wrong date input !!!" << endl;
+		exit(1);
+	}
+	else if((m == 4 || m == 6 || m ==  9|| m == 11) && d == 31)
+	{
+		cerr << "This month doesn't have 31 days !!!" << endl;
+		exit(1); 
+	}
+	else
+		day = d;
+		switch (m)
+		{
+		case 1:
+			month = "January";
+			break;
+		case 2:
+			month = "February";
+			break;
+		case 3:
+			month = "March";
+			break;
+		case 4:
+			month = "April";
+			break;
+		case 5:
+			month = "May";
+			break;
+		case 6:
+			month = "June";
+			break;
+		case 7:
+			month = "July";
+			break;
+		case 8:
+			month = "August";
+			break;
+		case 9:
+			month = "September";
+			break;
+		case 10:
+			month = "October";
+			break;
+		case 11:
+			month = "November";
+			break;
+		case 12:
+			month = "December";
+			break;
+		default:
+			cerr << "Wrong input !!!" << endl;
+			exit(1);
+			break;
+		}
+
+}
+
+
+void DayofYearSet::reallocate()
+{
+	DayofYear* new_set = new DayofYear[capacity];
+	
+	move_set(dataSet, new_set, sizeofSet);
+
+	delete [] dataSet;
+
+	dataSet = new_set;
+
+}
+
+void DayofYearSet::move_set(DayofYear* dest, DayofYear* src, size_t n)
+{
+  // Check if the arrays pointed to by dest and src overlap
+	if (dest >= src && dest < src + n)
+	{
+    // The arrays overlap. Use a temporary buffer to hold the elements.
+		std::unique_ptr<DayofYear[]> temp(new DayofYear[n]);
+
+    // Copy the elements from src to the temporary buffer
+		for (size_t i = 0; i < n; i++)
+			temp[i] = std::move(src[i]);
+
+    // Move the elements from the temporary buffer to dest
+    	for (size_t i = 0; i < n; i++)
+      		dest[i] = std::move(temp[i]);
+	}
+	else
+	{	// The arrays do not overlap. Use the original approach to move the elements.
+		if (dest < src)
+		{	// Move the elements in increasing order
+      		DayofYear *_dest = dest, *_src = src;
+      		for (size_t i = 0; i < n; i++)
+        		*_dest++ = std::move(*_src++);
+		}
+		else
+		{	// Move the elements in decreasing order
+      		DayofYear* _dest = dest + n - 1;
+      		DayofYear* _src = src + n - 1;
+      		for (size_t i = n; i > 0; i--)
+        		*_dest-- = std::move(*_src--);
+    	}
+  	}
+}
+
+
+
+DayofYearSet::DayofYearSet()
+{
+	dataSet = new DayofYear[capacity];
+}
 
 
 
